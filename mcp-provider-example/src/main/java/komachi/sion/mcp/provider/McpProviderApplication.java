03c838c962618caf67317ae8cbef6442fc870449
xiweng.yy <xiweng.yy@alibaba-inc.com>
2025-07-16 15:06:59 +0800

    Add example for mcp client request mcp server.

---
+++ mcp-provider-example/src/main/java/komachi/sion/mcp/provider/McpProviderApplication.java
@@ -0,0 +1,17 @@
+package komachi.sion.mcp.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ * @author xiweng.yy
+ */
+@SpringBootApplication
+public class McpProviderApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(McpProviderApplication.class, args);
+    }
+}

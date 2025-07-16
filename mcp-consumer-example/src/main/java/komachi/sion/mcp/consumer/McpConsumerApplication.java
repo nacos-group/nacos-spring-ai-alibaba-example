c930e82fcc08374ac249b53fde9c74579f5966ba
xiweng.yy <xiweng.yy@alibaba-inc.com>
2025-07-16 15:06:59 +0800

    Add example for mcp client request mcp server.

---
+++ mcp-consumer-example/src/main/java/komachi/sion/mcp/consumer/McpConsumerApplication.java
@@ -0,0 +1,17 @@
+package komachi.sion.mcp.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ * @author xiweng.yy
+ */
+@SpringBootApplication
+public class McpConsumerApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(McpConsumerApplication.class, args);
+    }
+}
---
+++ mcp-consumer-example/src/main/java/komachi/sion/mcp/consumer/controller/NacosController.java
@@ -0,0 +1,56 @@
+package komachi.sion.mcp.consumer.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.ChatClientResponse;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+/**
+ *
+ *
+ * @author xiweng.yy
+ */
+@RestController
+@RequestMapping("/nacos")
+public class NacosController {
+    
+    private final ChatClient dashScopeChatClient;
+    
+    public NacosController(ChatClient chatClient) {
+        this.dashScopeChatClient = chatClient;
+    }
+    
+    @GetMapping("/simple/chat")
+    public String simpleChat(
+            @RequestParam(value = "query", defaultValue = "你好，我的环境中有哪些Nacos集群？") String query) {
+        return dashScopeChatClient.prompt(query).call().content();
+    }
+    
+    /**
+     * ChatClient 流式调用
+     */
+    @GetMapping("/stream/chat")
+    public Flux<String> streamChat(
+            @RequestParam(value = "query", defaultValue = "你好，你好，我的环境中有哪些Nacos集群？") String query,
+            HttpServletResponse response) {
+        response.setCharacterEncoding("UTF-8");
+        Flux<ChatClientResponse> chatClientResponse = dashScopeChatClient.prompt(query).stream().chatClientResponse();
+        return chatClientResponse.mapNotNull(ChatClientResponse::chatResponse)
+                .map(r -> {
+                    if (r.getResult() == null || r.getResult().getOutput() == null
+                            || r.getResult().getOutput().getText() == null) {
+                        return "";
+                    }
+                    if (StringUtils.hasLength(r.getResult().getOutput().getText())) {
+                        return r.getResult().getOutput().getText();
+                    }
+                    return (String) r.getResult().getOutput().getMetadata().get("reasoningContent");
+                }).filter(StringUtils::hasLength);
+    }
+}

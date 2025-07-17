a4a00d9fa822122bbfc0c91e44c30fdc95e267f5
xiweng.yy <xiweng.yy@alibaba-inc.com>
2025-07-17 15:42:26 +0800

    通过Gateway将nacos的restAPI转变成mcp服务

---
+++ mcp-trans-from-http-example/src/main/java/komachi/sion/mcp/trans/McpTransFromHttpApplication.java
@@ -0,0 +1,16 @@
+package komachi.sion.mcp.trans;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ * @author xiweng.yy
+ */
+@SpringBootApplication
+public class McpTransFromHttpApplication {
+    
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(McpTransFromHttpApplication.class, args);
+    }
+}

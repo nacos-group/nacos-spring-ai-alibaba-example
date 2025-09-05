168f10f14e4cbb705e70f41cb0f1740af527153a
xiweng.yy <xiweng.yy@alibaba-inc.com>
2025-09-05 10:22:25 +0800

    Adapt studio observer.

--- a2a-example/a2a-agent-server-example/src/main/java/komachi/sion/a2a/server/A2aServerApplication.java
+++ a2a-example/a2a-agent-server-example/src/main/java/komachi/sion/a2a/server/A2aServerApplication.java
@@ -1,7 +1,10 @@
 package komachi.sion.a2a.server;
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  *
@@ -13,5 +16,10 @@ public class A2aServerApplication {
     
     public static void main(String[] args) {
         SpringApplication.run(A2aServerApplication.class, args);
+    }
+    
+    @Bean
+    public OpenTelemetry openTelemetry() {
+        return GlobalOpenTelemetry.get();
     }
 }
